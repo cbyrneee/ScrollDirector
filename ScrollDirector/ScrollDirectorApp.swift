d6bc867dae8472bf24778a070a86939ecb681f33
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-06-26 17:14:35 +0100

    Initial Commit

---
+++ ScrollDirector/ScrollDirectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollDirectorApp.swift
+//  ScrollDirector
+//
+//  Created by Conor Byrne on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrollDirectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
